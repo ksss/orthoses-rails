7d6c6ae6b93b627e2230f2e35371586f2a2eb955
ksss <co000ri@gmail.com>
2022-05-30 17:17:55 +0900

    Drop All

--- lib/orthoses/active_support.rb
+++ lib/orthoses/active_support.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'active_support/all'
 require_relative 'active_support/class_attribute'
 require_relative 'active_support/concern'
 require_relative 'active_support/delegation'
