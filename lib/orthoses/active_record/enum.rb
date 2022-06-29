695f95609811dc905b04b44725df94af6c42f635
ksss <co000ri@gmail.com>
2022-06-29 14:21:45 +0900

    Add ActiveRecord::Enum

---
+++ lib/orthoses/active_record/enum.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+module Orthoses
+  module ActiveRecord
+    # <= 6.1
+    #   def enum(definitions)
+    # >= 7.0
+    #   def enum(name = nil, values = nil, **options)
+    class Enum
+      def initialize(loader)
+        @loader = loader
+      end
+
+      def call
+        enum = CallTracer.new
+        store = enum.trace(::ActiveRecord::Enum.instance_method(:enum)) do
+          @loader.call
+        end
+
+        enum.captures.each do |capture|
+          base_name = Utils.module_name(capture.method.receiver) or next
+
+          if capture.argument[:definitions]
+            # on rails 6
+            definitions = capture.argument[:definitions].slice!(:_prefix, :_suffix, :_scopes, :_default)
+            options = capture.argument[:definitions].transform_keys { |key| :"#{key[1..-1]}" }
+            definitions.each { |name, values| _enum(store, base_name, name, values, **options) }
+          else
+            # on rails 7
+            name = capture.argument[:name]
+            values = capture.argument[:values]
+            options = capture.argument[:options]
+            if name
+              # rails 7 style
+              values, options = options, {} unless values
+              _enum(store, base_name, name, values, **options)
+            else
+              # rails 6 style
+              definitions = options.slice!(:_prefix, :_suffix, :_scopes, :_default)
+              options.transform_keys! { |key| :"#{key[1..-1]}" }
+
+              definitions.each { |name, values| _enum(store, base_name, name, values, **options) }
+            end
+          end
+        end
+
+        store
+      end
+
+      private
+
+      def _enum(store, base_name, name, values, prefix: nil, suffix: nil, **_options)
+        name = name.to_s
+
+        prefix = if prefix
+          prefix == true ? "#{name}_" : "#{prefix}_"
+        end
+
+        suffix = if suffix
+          suffix == true ? "_#{name}" : "_#{suffix}"
+        end
+
+        return_type_param = Hash === values ? "[String, String]" : "[String, Integer]"
+        store[base_name] << "def self.#{name.pluralize}: () -> ActiveSupport::HashWithIndifferentAccess#{return_type_param}"
+
+        pairs = values.respond_to?(:each_pair) ? values.each_pair : values.each_with_index
+        pairs.each do |label, value|
+          value_method_name = "#{prefix}#{label}#{suffix}"
+          enum_methods_content = store["#{base_name}::ActiveRecord_Enum_EnumMethods"]
+          enum_methods_content.header = "module #{base_name}::ActiveRecord_Enum_EnumMethods"
+          enum_methods_content << "def #{value_method_name}?: () -> bool"
+          enum_methods_content << "def #{value_method_name}!: () -> bool"
+        end
+      end
+    end
+  end
+end
---
+++ lib/orthoses/active_record/enum_test.rb
@@ -0,0 +1,138 @@
+module EnumTest
+  LOADER1 = ->(){
+    class User1 < ActiveRecord::Base
+      enum array: [ :array_active, :array_archived ]
+      enum hash: { hash_active: "active", hash_archived: "archived" }
+      enum pref: [ :active, :archived ], _prefix: true
+      enum suff: [ :active, :archived ], _suffix: true
+    end
+  }
+  def test_enum(t)
+    store = Orthoses::ActiveRecord::Enum.new(
+      Orthoses::Store.new(LOADER1)
+    ).call
+
+    actual = store["EnumTest::User1"].to_rbs
+    expect = <<~RBS
+      class EnumTest::User1 < ::ActiveRecord::Base
+        def self.arrays: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
+        def self.hashes: () -> ActiveSupport::HashWithIndifferentAccess[String, String]
+        def self.prefs: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
+        def self.suffs: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
+      end
+    RBS
+    unless expect == actual
+      t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
+    end
+
+    actual = store["EnumTest::User1::ActiveRecord_Enum_EnumMethods"].to_rbs
+    expect = <<~RBS
+      module EnumTest::User1::ActiveRecord_Enum_EnumMethods
+        def array_active?: () -> bool
+
+        def array_active!: () -> bool
+
+        def array_archived?: () -> bool
+
+        def array_archived!: () -> bool
+
+        def hash_active?: () -> bool
+
+        def hash_active!: () -> bool
+
+        def hash_archived?: () -> bool
+
+        def hash_archived!: () -> bool
+
+        def pref_active?: () -> bool
+
+        def pref_active!: () -> bool
+
+        def pref_archived?: () -> bool
+
+        def pref_archived!: () -> bool
+
+        def active_suff?: () -> bool
+
+        def active_suff!: () -> bool
+
+        def archived_suff?: () -> bool
+
+        def archived_suff!: () -> bool
+      end
+    RBS
+    unless expect == actual
+      t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
+    end
+  end
+
+  LOADER2 = ->(){
+    class User2 < ActiveRecord::Base
+      enum array: [ :array_active, :array_archived ]
+      enum hash: { hash_active: "active", hash_archived: "archived" }, _scopes: false
+    end
+  }
+  def test_enum_with_scope(t)
+    store = Orthoses::ActiveRecord::Enum.new(
+      Orthoses::ActiveRecord::Scope.new(
+        Orthoses::Store.new(LOADER2)
+      )
+    ).call
+
+    actual = store["EnumTest::User2"].to_rbs
+    expect = <<~RBS
+      class EnumTest::User2 < ::ActiveRecord::Base
+        def self.array_active: () -> EnumTest::User2::ActiveRecord_Relation
+        def self.not_array_active: () -> EnumTest::User2::ActiveRecord_Relation
+        def self.array_archived: () -> EnumTest::User2::ActiveRecord_Relation
+        def self.not_array_archived: () -> EnumTest::User2::ActiveRecord_Relation
+        def self.arrays: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
+        def self.hashes: () -> ActiveSupport::HashWithIndifferentAccess[String, String]
+      end
+    RBS
+    unless expect == actual
+      t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
+    end
+
+    actual = store["EnumTest::User2::GeneratedRelationMethods"].to_rbs
+    expect = <<~RBS
+      module EnumTest::User2::GeneratedRelationMethods
+        def array_active: () -> EnumTest::User2::ActiveRecord_Relation
+
+        def not_array_active: () -> EnumTest::User2::ActiveRecord_Relation
+
+        def array_archived: () -> EnumTest::User2::ActiveRecord_Relation
+
+        def not_array_archived: () -> EnumTest::User2::ActiveRecord_Relation
+      end
+    RBS
+    unless expect == actual
+      t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
+    end
+  end
+
+  if ActiveRecord::VERSION::MAJOR >= 7
+    LOADER3 = ->{
+      class User3 < ActiveRecord::Base
+        enum :array, [ :array_active, :array_archived ]
+        enum :hash, hash_active: "active", hash_archived: "archived"
+      end
+    }
+    def test_rails_7_style(t)
+      store = Orthoses::ActiveRecord::Enum.new(
+        Orthoses::Store.new(LOADER3)
+      ).call
+
+      actual = store["EnumTest::User3"].to_rbs
+      expect = <<~RBS
+        class EnumTest::User3 < ::ActiveRecord::Base
+          def self.arrays: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
+          def self.hashes: () -> ActiveSupport::HashWithIndifferentAccess[String, String]
+        end
+      RBS
+      unless expect == actual
+        t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
+      end
+    end
+  end
+end
