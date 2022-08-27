50892de4d886ec9cc32a0a1651c0aa50f3ae1be0
ksss <co000ri@gmail.com>
2022-08-27 22:16:06 +0900

    Drop each because it's not convenient.

--- lib/orthoses/active_support.rb
+++ lib/orthoses/active_support.rb
@@ -5,18 +5,3 @@ require_relative 'active_support/configurable'
 require_relative 'active_support/delegation'
 require_relative 'active_support/mattr_accessor'
 require_relative 'active_support/time_with_zone'
-
-module Orthoses
-  module ActiveSupport
-    # Interface for adding middleware in bulk.
-    #     Orthoses::ActiveSupport.each do |middleware, **args|
-    #       use middleware, **args
-    #     end
-    def self.each
-      yield ClassAttribute
-      yield Delegation
-      yield MattrAccessor
-      yield TimeWithZone
-    end
-  end
-end
