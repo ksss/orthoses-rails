05e4ff9f282ce7f096dddcafef481a9b118af8d5
ksss <co000ri@gmail.com>
2023-07-27 13:18:40 +0900

    Support v6.0 and alias_attribute

--- lib/orthoses/active_record/generated_attribute_methods.rb
+++ lib/orthoses/active_record/generated_attribute_methods.rb
@@ -3,6 +3,30 @@
 module Orthoses
   module ActiveRecord
     class GeneratedAttributeMethods
+      TARGET_TYPE_MAP = {
+        "attribute" => "() -> %<type>s",
+        "attribute=" => "(%<type>s) -> %<type>s",
+        "attribute?" => "() -> bool",
+        "attribute_before_last_save" => "() -> %<opt>s",
+        "attribute_before_type_cast" => "() -> %<type>s",
+        "attribute_came_from_user?" => "() -> bool",
+        "attribute_change" => "() -> [%<opt>s, %<opt>s]",
+        "attribute_change_to_be_saved" => "() -> Array[%<opt>s]?",
+        "attribute_changed?" => "() -> bool",
+        "attribute_for_database" => "() -> %<type>s",
+        "attribute_in_database" => "() -> %<opt>s",
+        "attribute_previous_change" => "() -> Array[%<opt>s]?",
+        "attribute_previously_changed?" => "() -> bool",
+        "attribute_previously_was" => "() -> %<opt>s",
+        "attribute_was" => "() -> %<opt>s",
+        "attribute_will_change!" => "() -> void",
+        "clear_attribute_change" => "() -> void",
+        "restore_attribute!" => "() -> void",
+        "saved_change_to_attribute" => "() -> Array[%<opt>s]?",
+        "saved_change_to_attribute?" => "() -> bool",
+        "will_save_change_to_attribute?" => "() -> bool",
+      }
+
       def initialize(loader)
         @loader = loader
       end
@@ -12,7 +36,7 @@ module Orthoses
           ::ActiveRecord::Base.descendants.each do |klass|
             next if klass.abstract_class?
 
-            name = Utils.module_name(klass) || next
+            base_klass_name = Utils.module_name(klass) || next
             begin
               klass.columns
             rescue ::ActiveRecord::StatementInvalid => e
@@ -20,36 +44,26 @@ module Orthoses
               next
             end
 
-            lines = klass.columns.flat_map do |col|
+            lines = []
+            klass.columns_hash.each do |name, col|
               req = ActiveRecord.sql_type_to_rbs(col.type)
               opt = "#{req}?"
               type = col.null ? opt : req
 
-              [
-                "def #{col.name}: () -> #{type}",
-                "def #{col.name}=: (#{type}) -> #{type}",
-                "def #{col.name}?: () -> bool",
-                "def #{col.name}_changed?: () -> bool",
-                "def #{col.name}_change: () -> [#{opt}, #{opt}]",
-                "def #{col.name}_will_change!: () -> void",
-                "def #{col.name}_was: () -> #{opt}",
-                "def #{col.name}_previously_changed?: () -> bool",
-                "def #{col.name}_previous_change: () -> Array[#{opt}]?",
-                "def #{col.name}_previously_was: () -> #{opt}",
-                "def #{col.name}_before_last_save: () -> #{opt}",
-                "def #{col.name}_change_to_be_saved: () -> Array[#{opt}]?",
-                "def #{col.name}_in_database: () -> #{opt}",
-                "def saved_change_to_#{col.name}: () -> Array[#{opt}]?",
-                "def saved_change_to_#{col.name}?: () -> bool",
-                "def will_save_change_to_#{col.name}?: () -> bool",
-                "def restore_#{col.name}!: () -> void",
-                "def clear_#{col.name}_change: () -> void",
-              ]
+              ::ActiveRecord::Base.attribute_method_matchers.each do |matcher|
+                tmpl = TARGET_TYPE_MAP[matcher.target]
+                lines << "def #{matcher.method_name(name)}: #{tmpl % {type: type, opt: opt}}"
+              end
             end
-            generated_attribute_methods = "#{name}::AttributeMethods::GeneratedAttributeMethods"
-            store[name] << "include #{generated_attribute_methods}"
+            klass.attribute_aliases.each do |alias_name, column_name|
+              ::ActiveRecord::Base.attribute_method_matchers.each do |matcher|
+                lines << "alias #{matcher.method_name(alias_name)} #{matcher.method_name(column_name)}"
+              end
+            end
 
-            store["#{name}::AttributeMethods"].header = "module #{name}::AttributeMethods"
+            generated_attribute_methods = "#{base_klass_name}::GeneratedAttributeMethods"
+            store[base_klass_name] << "include #{generated_attribute_methods}"
+
             store[generated_attribute_methods].header = "module #{generated_attribute_methods}"
             store[generated_attribute_methods].concat(lines)
           end
--- lib/orthoses/active_record/generated_attribute_methods_test.rb
+++ lib/orthoses/active_record/generated_attribute_methods_test.rb
@@ -5,6 +5,7 @@ require_relative '../../../test/fake_schema'
 module GeneratedAttributeMethodsTest
   LOADER = -> () {
     class User < ActiveRecord::Base
+      alias_attribute :nickname, :name
     end
     Class.new(ActiveRecord::Base)
   }
@@ -33,120 +34,16 @@ module GeneratedAttributeMethodsTest
       t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
     end
 
-    expect = <<~RBS
-      module GeneratedAttributeMethodsTest::User::AttributeMethods::GeneratedAttributeMethods
-        def id: () -> ::Integer
-
-        def id=: (::Integer) -> ::Integer
-
-        def id?: () -> bool
-
-        def id_changed?: () -> bool
-
-        def id_change: () -> [ ::Integer?, ::Integer? ]
-
-        def id_will_change!: () -> void
-
-        def id_was: () -> ::Integer?
-
-        def id_previously_changed?: () -> bool
-
-        def id_previous_change: () -> Array[::Integer?]?
-
-        def id_previously_was: () -> ::Integer?
-
-        def id_before_last_save: () -> ::Integer?
-
-        def id_change_to_be_saved: () -> Array[::Integer?]?
-
-        def id_in_database: () -> ::Integer?
-
-        def saved_change_to_id: () -> Array[::Integer?]?
-
-        def saved_change_to_id?: () -> bool
-
-        def will_save_change_to_id?: () -> bool
-
-        def restore_id!: () -> void
-
-        def clear_id_change: () -> void
-
-        def name: () -> ::String
-
-        def name=: (::String) -> ::String
-
-        def name?: () -> bool
-
-        def name_changed?: () -> bool
-
-        def name_change: () -> [ ::String?, ::String? ]
-
-        def name_will_change!: () -> void
-
-        def name_was: () -> ::String?
-
-        def name_previously_changed?: () -> bool
-
-        def name_previous_change: () -> Array[::String?]?
-
-        def name_previously_was: () -> ::String?
-
-        def name_before_last_save: () -> ::String?
-
-        def name_change_to_be_saved: () -> Array[::String?]?
-
-        def name_in_database: () -> ::String?
-
-        def saved_change_to_name: () -> Array[::String?]?
-
-        def saved_change_to_name?: () -> bool
-
-        def will_save_change_to_name?: () -> bool
-
-        def restore_name!: () -> void
-
-        def clear_name_change: () -> void
-
-        def confirmed: () -> bool?
-
-        def confirmed=: (bool?) -> bool?
-
-        def confirmed?: () -> bool
-
-        def confirmed_changed?: () -> bool
-
-        def confirmed_change: () -> [ bool?, bool? ]
-
-        def confirmed_will_change!: () -> void
-
-        def confirmed_was: () -> bool?
-
-        def confirmed_previously_changed?: () -> bool
-
-        def confirmed_previous_change: () -> Array[bool?]?
-
-        def confirmed_previously_was: () -> bool?
-
-        def confirmed_before_last_save: () -> bool?
-
-        def confirmed_change_to_be_saved: () -> Array[bool?]?
-
-        def confirmed_in_database: () -> bool?
-
-        def saved_change_to_confirmed: () -> Array[bool?]?
-
-        def saved_change_to_confirmed?: () -> bool
-
-        def will_save_change_to_confirmed?: () -> bool
-
-        def restore_confirmed!: () -> void
-
-        def clear_confirmed_change: () -> void
-      end
-    RBS
     actual = store["GeneratedAttributeMethodsTest::User::AttributeMethods::GeneratedAttributeMethods"].to_rbs
-    unless expect == actual
-      t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
+    [
+      "def id: () -> ::Integer",
+      "def name: () -> ::String",
+      "def confirmed: () -> bool?",
+      "alias nickname name"
+    ].each do |check|
+      unless actual.include?(check)
+        t.error("should include `#{check}`. But, not.")
+      end
     end
   end
 end
