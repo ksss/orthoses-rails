038b8a2fdd3bcf51d3f8c437f268f320e8e20f3b
ksss <co000ri@gmail.com>
2024-08-31 14:36:02 +0900

    Fix spec

--- test/fake_schema.rb
+++ test/fake_schema.rb
@@ -1,3 +1,24 @@
+FakeColumn = Struct.new(:type, :name, :null, keyword_init: true)
+def FakeColumn.attribute_types
+  {
+    "id" => FakeColumn.new(
+      name: :id,
+      null: false,
+      type: :integer,
+    ),
+    "name" => FakeColumn.new(
+      name: :name,
+      null: false,
+      type: :string,
+    ),
+    "confirmed" => FakeColumn.new(
+      name: :confirmed,
+      null: true,
+      type: :boolean,
+    ),
+  }
+end
+
 module ::ActiveRecord
   module AttributeMethods::PrimaryKey::ClassMethods
     def get_primary_key(base_name)
@@ -6,29 +27,19 @@ module ::ActiveRecord
   end
 
   module ModelSchema::ClassMethods
-    FakeColumn = Struct.new(:type, :name, :null, keyword_init: true)
 
-    def load_schema
+    def attribute_types
+      FakeColumn.attribute_types
     end
+  end
+end
 
-    def columns_hash
-      {
-        id: FakeColumn.new(
-          name: :id,
-          null: false,
-          type: :integer,
-        ),
-        name: FakeColumn.new(
-          name: :name,
-          null: false,
-          type: :string,
-        ),
-        confirmed: FakeColumn.new(
-          name: :confirmed,
-          null: true,
-          type: :boolean,
-        ),
-      }
+module ActiveModel
+  module AttributeRegistration
+    module ClassMethods
+      def attribute_types
+        FakeColumn.attribute_types
+      end
     end
   end
 end
