0d3085f891e767c4dcbb912cf428b8731970d7ce
ksss <co000ri@gmail.com>
2025-01-31 14:20:23 +0900

    Support unstrict enum union type
    Too many union types are steep and there are concerns about performance degradation.

--- lib/orthoses/active_record/enum_test.rb
+++ lib/orthoses/active_record/enum_test.rb
@@ -13,9 +13,18 @@ module EnumTest
       enum :escape, [:"a-b-c", :"e_[]_f"]
     end
   }
+
+  LOADER2 = ->(){
+    class User2 < ActiveRecord::Base
+      enum :array, [ :array_active, :array_archived ]
+      enum :map, { map_active: "active", map_archived: "archived" }
+    end
+  }
+
   def test_enum(t)
     store = Orthoses::ActiveRecord::Enum.new(
-      Orthoses::Store.new(LOADER1)
+      Orthoses::Store.new(LOADER1),
+      strict: false
     ).call
 
     actual = store["EnumTest::User1"].to_rbs
@@ -23,30 +32,25 @@ module EnumTest
       class EnumTest::User1 < ::ActiveRecord::Base
         include EnumTest::User1::ActiveRecord_Enum_EnumMethods
         def self.arrays: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
-        def array: () -> ("array_active" | "array_archived")
-        def array=: (:array_active | :array_archived) -> void
-                  | ("array_active" | "array_archived") -> void
-                  | (0 | 1) -> void
+        def array: () -> String
+        def array=: (Symbol | String) -> void
+                  | (Integer) -> void
         def self.maps: () -> ActiveSupport::HashWithIndifferentAccess[String, String]
-        def map: () -> ("map_active" | "map_archived")
-        def map=: (:map_active | :map_archived) -> void
-                | ("map_active" | "map_archived") -> void
-                | ("active" | "archived") -> void
+        def map: () -> String
+        def map=: (Symbol | String) -> void
+                | (String) -> void
         def self.prefs: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
-        def pref: () -> ("active" | "archived")
-        def pref=: (:active | :archived) -> void
-                 | ("active" | "archived") -> void
-                 | (0 | 1) -> void
+        def pref: () -> String
+        def pref=: (Symbol | String) -> void
+                 | (Integer) -> void
         def self.suffs: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
-        def suff: () -> ("active" | "archived")
-        def suff=: (:active | :archived) -> void
-                 | ("active" | "archived") -> void
-                 | (0 | 1) -> void
+        def suff: () -> String
+        def suff=: (Symbol | String) -> void
+                 | (Integer) -> void
         def self.escapes: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
-        def escape: () -> ("a-b-c" | "e_[]_f")
-        def escape=: (Symbol) -> void
-                   | ("a-b-c" | "e_[]_f") -> void
-                   | (0 | 1) -> void
+        def escape: () -> String
+        def escape=: (Symbol | String) -> void
+                   | (Integer) -> void
       end
     RBS
     unless expect == actual
@@ -103,6 +107,55 @@ module EnumTest
         def e___f?: () -> bool
 
         def e___f!: () -> bool
+      end
+    RBS
+    unless expect == actual
+      t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
+    end
+
+    store = Orthoses::ActiveRecord::Enum.new(
+      Orthoses::Store.new(LOADER2),
+      strict: true
+    ).call
+
+    actual = store["EnumTest::User2"].to_rbs
+    expect = <<~RBS
+      class EnumTest::User2 < ::ActiveRecord::Base
+        include EnumTest::User2::ActiveRecord_Enum_EnumMethods
+        def self.arrays: () -> ActiveSupport::HashWithIndifferentAccess[String, Integer]
+        def array: () -> ("array_active" | "array_archived")
+        def array=: (:array_active | :array_archived) -> void
+                  | ("array_active" | "array_archived") -> void
+                  | (0 | 1) -> void
+        def self.maps: () -> ActiveSupport::HashWithIndifferentAccess[String, String]
+        def map: () -> ("map_active" | "map_archived")
+        def map=: (:map_active | :map_archived) -> void
+                | ("map_active" | "map_archived") -> void
+                | ("active" | "archived") -> void
+      end
+    RBS
+    unless expect == actual
+      t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
+    end
+
+    actual = store["EnumTest::User2::ActiveRecord_Enum_EnumMethods"].to_rbs
+    expect = <<~RBS
+      module EnumTest::User2::ActiveRecord_Enum_EnumMethods
+        def array_active?: () -> bool
+
+        def array_active!: () -> bool
+
+        def array_archived?: () -> bool
+
+        def array_archived!: () -> bool
+
+        def map_active?: () -> bool
+
+        def map_active!: () -> bool
+
+        def map_archived?: () -> bool
+
+        def map_archived!: () -> bool
       end
     RBS
     unless expect == actual
